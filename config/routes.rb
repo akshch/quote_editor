3f2e516c0addfd9ea02ded0c49697b3355759d7c
Akshaykumar Chakkarkota <akshay.chakkarkota@cuelinks>
2022-05-16 00:21:05 +0530

    line item controller and CRUD added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :quotes
+  resources :quotes do
+    resources :line_item_dates, except: [:index, :show]
+  end
   # Defines the root path route ("/")
   root to: "pages#home"
 end
